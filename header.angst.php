2de0d8949a881032d7f5d067a3b803e871712ceb
Scott Wilder Olesen <swo@mit.edu>
2014-04-29 14:59:17 -0400

    init

---
+++ header.angst.php
@@ -0,0 +1,192 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xml:lang="en" xmlns="http://www.w3.org/1999/xhtml"><head>
+
+
+
+
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    
+    <meta name="Generator" content="iWeb 2.0.4">
+    <meta name="iWeb-Build" content="local-build-20090405">
+    <meta name="viewport" content="width=700">
+    <title>AnGST Online Server</title>
+    <link rel="stylesheet" type="text/css" media="screen,print" href="files/5_AnGST_Online_Server.css">
+    <!--[if IE]><link rel='stylesheet' type='text/css' media='screen,print' href='files/5_AnGST_Online_ServerIE.css'/><![endif]--><style type="text/css">
+/*<![CDATA[*/
+	@import "../../../../Scripts/Widgets/HTMLRegion/Paste.css";
+/*]]>*/
+</style>
+    <script language="javascript" type="text/javascript"><!--
+
+iWebBlogMainPageName = 'Software.html';
+iWebBlogMainPageTitle = 'Software';
+iWebBlogArchivePageName = 'Archive.html';
+iWebBlogArchivePageTitle = 'Archive';
+iWebBlogFeedType = 'static';
+
+
+--></script>
+    <script type="text/javascript" src="files/iWebSite.js"></script>
+    <script type="text/javascript" src="files/WidgetCommon.js"></script>
+    <script type="text/javascript" src="files/navbar.js"></script>
+    <script type="text/javascript" src="files/search.js"></script><script src="files/loc.js" type="text/javascript" charset="utf-8"></script><script src="files/utility.js" type="text/javascript" charset="utf-8"></script>
+    <script type="text/javascript" src="files/iWebBlog.js"></script>
+    <script type="text/javascript" src="files/iWebImage.js"></script>
+    <script type="text/javascript" src="files/Paste.js"></script>
+    <script type="text/javascript" src="files/5_AnGST_Online_Server.js"></script>
+    <script type="text/javascript" src="jquery-1.3.2.min.js"></script>
+  <style type="text/css">div#widget0 .navbar {
+	font-family: 'Helvetica Neue', Arial, sans-serif;
+	font-size: .8em;
+	color: #666666;
+	line-height: 30px;
+	border-bottom: 3px solid #ccc;
+}
+
+div#widget0 .navbar-bg {
+	text-align: right;}
+
+div#widget0 .navbar-bg ul {
+	list-style: none;
+	margin: 0px;
+	padding: 0px;
+}
+
+
+div#widget0 li {
+	list-style-type: none;
+	display: inline;
+	padding: 0px 5px 0px 0px;
+}
+
+
+div#widget0 li a {
+	text-decoration: none;
+	padding: 10px;
+	color: #666666;
+	font-weight: bold;
+}
+
+div#widget0 li a:visited {
+	text-decoration: none;
+	padding: 10px;
+	color: #666666;
+	font-weight: bold;
+}
+
+div#widget0 li a:hover
+{
+
+ 	color: #999999;
+	text-decoration: none;
+}
+
+
+div#widget0 li.current-page a
+{
+	 color: #66ABC5;
+	text-decoration: none;
+}
+</style>
+
+<link href="files/5_AnGST_Online_ServerMoz.css" type="text/css" rel="stylesheet"></head><div FirebugVersion="1.3.3" style="display: none;" id="_firebugConsole"></div><body style="margin: 0pt; background: rgb(81, 81, 81) none repeat scroll 0% 0%; -moz-background-clip: -moz-initial; -moz-background-origin: -moz-initial; -moz-background-inline-policy: -moz-initial;" onload="onPageLoad();" onunload="onPageUnload();">
+    <div style="text-align: center;">
+        <div style="margin: 10px auto; height: 1470px; background: rgb(255, 255, 255) none repeat scroll 0% 0%; overflow: visible; position: relative; -moz-background-clip: -moz-initial; -moz-background-origin: -moz-initial; -moz-background-inline-policy: -moz-initial; text-align: left; width: 700px;" id="body_content">
+        <div style="margin-left: 0px; position: relative; width: 700px; z-index: 0;" id="nav_layer">
+          <div style="height: 0px; line-height: 0px;" class="bumper">&nbsp;</div>
+          <div class="com-apple-iweb-widget-navbar flowDefining" id="widget0" style="margin-left: 0px; margin-top: 0px; position: relative; width: 700px; z-index: 1;">
+    
+            <div id="widget0-navbar" class="navbar">
+
+      
+              <div id="widget0-bg" class="navbar-bg">
+
+        
+                <ul id="widget0-navbar-list" class="navbar-list">
+		<li class="noncurrent-page"><a href="http://almlab.mit.edu">ALM </a></li>
+		<li class="noncurrent-page"><a href="http://almlab.mit.edu/ealm.html">Eric&nbsp;Alm </a></li>
+		<li class="noncurrent-page"><a href="http://almlab.mit.edu/members.html">Lab&nbsp;Members </a></li>
+		<li class="current-page"><a href="http://almlab.mit.edu/software.html">Software </a></li>
+		<li class="noncurrent-page"><a href="http://almlab.mit.edu/publications.html">Publications </a></li>
+		<li class="noncurrent-page"><a href="http://almlab.mit.edu/press.html">News </a></li>
+		<li class="noncurrent-page"><a href="http://almlab.mit.edu/contact.html">Contact </a></li></ul>
+                
+      
+</div>
+              
+    
+</div>
+          </div>
+          <script type="text/javascript"><!--//--><![CDATA[//><!--
+new NavBar('widget0', '../../../../Scripts/Widgets/Navbar', '../../../../Scripts/Widgets/SharedResources', '../../../..', {"current-page-GUID": "42B659BB-78E8-446D-A218-89B458A303E4", "path-to-root": "..\/..\/..\/..\/", "isCollectionPage": "YES", "navbar-css": ".navbar {\n\tfont-family: 'Helvetica Neue', Arial, sans-serif;\n\tfont-size: .8em;\n\tcolor: #666666;\n\tline-height: 30px;\n\tborder-bottom: 3px solid #ccc;\n}\n\n.navbar-bg {\n\ttext-align: right;}\n\n.navbar-bg ul {\n\tlist-style: none;\n\tmargin: 0px;\n\tpadding: 0px;\n}\n\n\nli {\n\tlist-style-type: none;\n\tdisplay: inline;\n\tpadding: 0px 5px 0px 0px;\n}\n\n\nli a {\n\ttext-decoration: none;\n\tpadding: 10px;\n\tcolor: #666666;\n\tfont-weight: bold;\n}\n\nli a:visited {\n\ttext-decoration: none;\n\tpadding: 10px;\n\tcolor: #666666;\n\tfont-weight: bold;\n}\n\nli a:hover\r{\r\n \tcolor: #999999;\n\ttext-decoration: none;\r}\n\n\nli.current-page a\r{\r\t color: #66ABC5;\n\ttext-decoration: none;\r}\n"});
+//--><!]]></script>
+          <div style="clear: both; height: 0px; line-height: 0px;" class="spacer">&nbsp;</div>
+        </div>
+        <div style="height: 75px; margin-left: 0px; position: relative; width: 700px; z-index: 10;" id="header_layer">
+          <div style="height: 0px; line-height: 0px;" class="bumper">&nbsp;</div>
+          <div style="height: 40px; left: 610px; position: absolute; top: 24px; width: 78px; z-index: 1;" class="tinyText">
+            <div style="position: relative; width: 78px;">
+              <a href="http://www.mit.edu/" title="http://www.mit.edu"><img src="files/shapeimage_1.png" alt="" style="height: 38px; left: 0px; position: absolute; top: 0px; width: 72px;"></a>
+            </div>
+          </div>
+          
+
+
+          <div style="height: 102px; left: 10px; position: absolute; top: 13px; width: 556px; z-index: 1;" class="tinyText style_SkipStroke">
+            <img src="files/droppedImage.png" alt="" style="border: medium none ; height: 102px; width: 556px;">
+          </div>
+        </div>
+        <div style="margin-left: 0px; position: relative; width: 700px; z-index: 5; height: 1200px;" id="body_layer">
+          <div style="height: 0px; line-height: 0px;" class="bumper">&nbsp;</div>
+          <div style="height: 210px; left: 20px; position: absolute; top: 67px; width: 395px; z-index: 1;" class="tinyText stroke_0">
+            <div style="position: relative; width: 395px;">
+              <img src="files/shapeimage_2.jpg" alt="" style="height: 210px; left: 0px; position: absolute; top: 0px; width: 395px; visibility: visible;" id="generic-picture-attributes">
+            </div>
+          <img src="files/stroke.png" style="position: absolute; left: -5px; top: 5px; width: 10px; height: 200px;"><img src="files/stroke_1.png" style="position: absolute; left: -5px; top: -5px; width: 10px; height: 10px;"><img src="files/stroke_2.png" style="position: absolute; left: 5px; top: -5px; width: 385px; height: 10px;"><img src="files/stroke_3.png" style="position: absolute; left: 390px; top: -5px; width: 10px; height: 10px;"><img src="files/stroke_4.png" style="position: absolute; left: 390px; top: 5px; width: 10px; height: 200px;"><img src="files/stroke_5.png" style="position: absolute; left: 390px; top: 205px; width: 10px; height: 10px;"><img src="files/stroke_6.png" style="position: absolute; left: 5px; top: 205px; width: 385px; height: 10px;"><img src="files/stroke_7.png" style="position: absolute; left: -5px; top: 205px; width: 10px; height: 10px;"></div>
+          <div style="height: 1px; line-height: 1px;" class="tinyText">&nbsp;</div>
+          <div style="visibility: visible; margin-left: 43px; margin-top: 1126px; position: relative; width: 601px; z-index: 1;" class="style_SkipStroke_1 flowDefining">
+            <div class="text-content style_External_601_30" style="padding: 0px;">
+              <div class="style"></div>
+              <div style="height: 30px; line-height: 30px;" class="tinyText">&nbsp;</div>
+            </div>
+            <div id="generic-body-attributes" class="Free_Form"></div>
+          </div>
+          
+
+
+          <div id="id1" style="visibility: visible; height: 20px; left: 50px; position: absolute; top: 312px; width: 295px; z-index: 1;" class="style_SkipStroke_2">
+            <div class="text-content style_External_295_20" style="padding: 0px;">
+              <div class="style_1">
+                <p style="padding-bottom: 0pt; padding-top: 0pt;" class="Summary_Title">AnGST Online Server</p>
+              </div>
+            </div>
+            <div id="generic-title-attributes" class="Summary_Title"></div>
+          </div>
+          
+
+
+          <div id="id3" style="visibility: visible; height: 20px; left: 315px; position: absolute; top: 312px; width: 104px; z-index: 1;" class="style_SkipStroke_3">
+            <div class="text-content style_External_104_20" style="padding: 0px;">
+              <div class="style_1">
+                <p style="padding-bottom: 0pt; padding-top: 0pt;" class="paragraph_style" id="id2">4/5/09</p>
+              </div>
+            </div>
+            <div id="generic-datefield-attributes" class="Date"></div>
+          </div>
+          
+
+
+          <div id="id4" style="height: 220px; left: 421px; position: absolute; top: 62px; width: 269px; z-index: 1;" class="style_SkipStroke_4">
+            <div class="text-content style_External_269_220" style="padding: 0px;">
+              <div class="style_2">
+                <p style="padding-top: 0pt;" class="paragraph_style_1"><br></p>
+                <p style="padding-bottom: 0pt;" class="paragraph_style_1">Analyzer of Gene and Species Trees.</p>
+              </div>
+            </div>
+          </div>
+          
+
+          <div class="com-apple-iweb-widget-HTMLRegion" id="widget1" style="height: 1000px; left: 10px; position: absolute; top: 359px; width: 668px; z-index: 1;">
+            
+          <iframe id="widget1-frame" style="width: 0%; height: 0%;" marginheight="0" marginwidth="0" allowtransparency="true" scrolling="no" frameborder="0"></iframe>
+
